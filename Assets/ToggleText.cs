7dee0eedc3945a332d6d2dcd7a8e7c307c1e6a45
goondrious <srs_34@hotmail.com>
2021-06-03 20:57:35 -0400

    workflow test

---
+++ Assets/ToggleText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ToggleText : MonoBehaviour
+{
+    public void ToggleCanvasText() {
+		Text text = GameObject.Find("CanvasText").GetComponent<Text>();
+		text.enabled = !text.enabled;
+    }
+}
